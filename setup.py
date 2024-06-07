2d995fc2d7403633f1ccb5baa95cd05fe4979c53
bazor99 <k1ngx13y@gmail.com>
2024-06-07 19:26:04 +0100

    complete setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup 
+
+setup(
+    name="Generative AI project",
+    version="0.0.1",
+    author="Kingsley Odenigbo",
+    author_email="k1ngx13y@gmail.com",
+    description="GenAI interview creation",
+    packages=find_packages(),
+    install_requires=[]      
+)
